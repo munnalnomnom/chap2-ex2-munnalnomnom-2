1f03136e18e7ba20e265dbaa54936423c0a2d1a6
munnalnomnom <163238344+munnalnomnom@users.noreply.github.com>
2024-03-27 15:57:21 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-
+#include <math.h>
 int main()
 {
-
+    int hours,minutes;
+    printf("Input hours: ");
+    scanf("%d",&hours);
+    printf("Input minutes: ");
+    scanf("%d",&minutes);
+    minutes = minutes + hours*60;
+    printf("Total: %d minutes.",minutes);
     return 0;
 }
